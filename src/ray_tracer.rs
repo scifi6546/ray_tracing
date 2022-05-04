0711f9e5e0f69c4954e831169b9506f5f81236ed
Nicholas Alexeev <nickalexeev@protonmail.com>
2022-05-04 07:43:42 -0800

    Update ray_tracer.rs

--- src/ray_tracer.rs
+++ src/ray_tracer.rs
@@ -54,6 +54,54 @@ impl Material for Metal {
         }
     }
 }
+struct Dielectric {
+    pub index_refraction: f32,
+}
+impl Dielectric {
+    fn refract(uv: Vector3<f32>, n: Vector3<f32>, etai_over_etat: f32) -> Vector3<f32> {
+        let cos_theta = n.dot(-1.0 * uv).min(1.0);
+
+        let r_out_perp = etai_over_etat * (uv + cos_theta * n);
+        let r_out_parallel = -1.0 * (1.0 - r_out_perp.dot(r_out_perp)).abs().sqrt() * n;
+        r_out_perp + r_out_parallel
+    }
+    fn reflectance(cosine: f32, ref_idx: f32) -> f32 {
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * ((1.0 - cosine).powi(5))
+    }
+}
+impl Material for Dielectric {
+    fn scatter(&self, ray_in: Ray, record_in: &HitRecord) -> Option<(RgbColor, Ray)> {
+        let refraction_ratio = if record_in.front_face {
+            1.0 / self.index_refraction
+        } else {
+            self.index_refraction
+        };
+        let unit_direction = ray_in.direction.normalize();
+        let cos_theta = record_in.normal.dot(-1.0 * unit_direction).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        let can_not_refract = (refraction_ratio * sin_theta) > 1.0;
+        let direction = if can_not_refract
+            || Self::reflectance(cos_theta, refraction_ratio) > rand::random::<f32>()
+        {
+            reflect(unit_direction, record_in.normal)
+        } else {
+            Self::refract(unit_direction, record_in.normal, refraction_ratio)
+        };
+
+        return Some((
+            RgbColor {
+                red: 1.0,
+                green: 1.0,
+                blue: 1.0,
+            },
+            Ray {
+                origin: record_in.position,
+                direction,
+            },
+        ));
+    }
+}
 pub fn rand_unit_vec() -> Vector3<f32> {
     loop {
         let v = 2.0 * (rand_vec() - Vector3::new(0.5, 0.5, 0.5));
@@ -336,13 +384,8 @@ impl RayTracer {
                         y: -0.1,
                         z: -1.0,
                     },
-                    material: Rc::new(RefCell::new(Metal {
-                        albedo: RgbColor {
-                            red: 0.9,
-                            green: 0.6,
-                            blue: 0.2,
-                        },
-                        fuzz: 0.6,
+                    material: Rc::new(RefCell::new(Dielectric {
+                        index_refraction: 1.5,
                     })),
                 },
                 Sphere {
