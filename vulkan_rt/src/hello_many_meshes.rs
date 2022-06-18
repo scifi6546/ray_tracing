42d0f1ac8507962aa104a4fe6fdf51f95144ee50
Nicholas Alexeev <nickalexeev@protonmail.com>
2022-06-18 15:13:25 -0800

    finished render model

--- vulkan_rt/src/hello_many_meshes.rs
+++ vulkan_rt/src/hello_many_meshes.rs
@@ -30,6 +30,7 @@ struct RenderModel {
     texture_allocation: Allocation,
     texture_image_view: vk::ImageView,
     sampler: vk::Sampler,
+    num_indices: u32,
 }
 impl RenderModel {
     pub unsafe fn free_resources(self, base: &Base, allocator: &mut Allocator) {
@@ -165,14 +166,7 @@ impl Model {
         };
         let image_buffer_memory_req =
             unsafe { base.device.get_buffer_memory_requirements(image_buffer) };
-        let image_buffer_memory_index = unsafe {
-            find_memory_type_index(
-                &image_buffer_memory_req,
-                &base.device_memory_properties,
-                vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-            )
-            .expect("failed to find memory index")
-        };
+
         let image_buffer_allocation = allocator
             .allocate(&AllocationCreateDesc {
                 name: "image buffer allocation",
@@ -393,6 +387,7 @@ impl Model {
             texture_allocation,
             texture_image_view,
             sampler,
+            num_indices: self.mesh.indices.len() as u32,
         }
     }
 }
@@ -504,378 +499,6 @@ pub fn run(base: &Base) {
     }
     .build_render_model(base, &mut allocator, &descriptor_pool, &desc_set_layouts);
 
-    let mesh = Mesh::sphere(64, 32);
-
-    let index_buffer_info = vk::BufferCreateInfo::builder()
-        .size(size_of::<u32>() as u64 * mesh.indices.len() as u64)
-        .usage(vk::BufferUsageFlags::INDEX_BUFFER)
-        .sharing_mode(vk::SharingMode::EXCLUSIVE);
-    let index_buffer = unsafe {
-        base.device
-            .create_buffer(&index_buffer_info, None)
-            .expect("failed to create index buffer")
-    };
-    let index_buffer_memory_req =
-        unsafe { base.device.get_buffer_memory_requirements(index_buffer) };
-    let index_buffer_memory = allocator
-        .allocate(&AllocationCreateDesc {
-            name: "index buffer memory",
-            requirements: index_buffer_memory_req,
-            location: MemoryLocation::CpuToGpu,
-            linear: true,
-        })
-        .expect("failed to allocate");
-    let index_ptr = index_buffer_memory.mapped_ptr().expect("failed to map ptr");
-
-    let mut index_slice = unsafe {
-        Align::new(
-            index_ptr.as_ptr(),
-            align_of::<u32>() as u64,
-            index_buffer_memory_req.size,
-        )
-    };
-    unsafe {
-        index_slice.copy_from_slice(&mesh.indices);
-
-        base.device
-            .bind_buffer_memory(
-                index_buffer,
-                index_buffer_memory.memory(),
-                index_buffer_memory.offset(),
-            )
-            .unwrap();
-    }
-
-    let vertex_input_buffer_info = vk::BufferCreateInfo::builder()
-        .size(size_of::<Vertex>() as u64 * mesh.vertices.len() as u64)
-        .usage(vk::BufferUsageFlags::VERTEX_BUFFER)
-        .sharing_mode(vk::SharingMode::EXCLUSIVE);
-    let vertex_input_buffer = unsafe {
-        base.device
-            .create_buffer(&vertex_input_buffer_info, None)
-            .expect("failed to create vertex input buffer")
-    };
-    let vertex_input_buffer_memory_req = unsafe {
-        base.device
-            .get_buffer_memory_requirements(vertex_input_buffer)
-    };
-    let vertex_buffer_allocation = allocator
-        .allocate(&AllocationCreateDesc {
-            requirements: vertex_input_buffer_memory_req,
-            location: MemoryLocation::CpuToGpu,
-            linear: true,
-            name: "Vertex Buffer",
-        })
-        .expect("failed to allocate");
-    let vert_ptr = vertex_buffer_allocation
-        .mapped_ptr()
-        .expect("failed to map vertex ptr");
-
-    unsafe {
-        let mut slice = Align::new(
-            vert_ptr.as_ptr(),
-            align_of::<Vertex>() as u64,
-            vertex_input_buffer_memory_req.size,
-        );
-        slice.copy_from_slice(&mesh.vertices);
-
-        base.device
-            .bind_buffer_memory(
-                vertex_input_buffer,
-                vertex_buffer_allocation.memory(),
-                vertex_buffer_allocation.offset(),
-            )
-            .expect("failed to bind vertex buffer memory");
-    }
-    let uniform_color_buffer_data = Vector4::new(1.0, 1.0, 1.0, 0.0);
-    let uniform_color_buffer_info = vk::BufferCreateInfo::builder()
-        .size(size_of_val(&uniform_color_buffer_data) as u64)
-        .usage(vk::BufferUsageFlags::UNIFORM_BUFFER)
-        .sharing_mode(vk::SharingMode::EXCLUSIVE);
-    let uniform_color_buffer = unsafe {
-        base.device
-            .create_buffer(&uniform_color_buffer_info, None)
-            .expect("failed to create color uniform buffer")
-    };
-    let uniform_color_buffer_memory_req = unsafe {
-        base.device
-            .get_buffer_memory_requirements(uniform_color_buffer)
-    };
-    let uniform_color_buffer_memory_index = unsafe {
-        find_memory_type_index(
-            &uniform_color_buffer_memory_req,
-            &base.device_memory_properties,
-            vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-        )
-        .expect("failed to find suitible memory type index")
-    };
-    let uniform_color_buffer_allocate_info = vk::MemoryAllocateInfo::builder()
-        .allocation_size(uniform_color_buffer_memory_req.size)
-        .memory_type_index(uniform_color_buffer_memory_index);
-    let uniform_color_buffer_memory = unsafe {
-        base.device
-            .allocate_memory(&uniform_color_buffer_allocate_info, None)
-            .expect("failed to allocate uniform color buffer memory")
-    };
-    unsafe {
-        let uniform_ptr = base
-            .device
-            .map_memory(
-                uniform_color_buffer_memory,
-                0,
-                uniform_color_buffer_memory_req.size,
-                vk::MemoryMapFlags::empty(),
-            )
-            .expect("failed to map uniform buffer memory");
-        let mut uniform_aligned_slice = Align::new(
-            uniform_ptr,
-            align_of::<Vector4>() as u64,
-            uniform_color_buffer_memory_req.size,
-        );
-        uniform_aligned_slice.copy_from_slice(&[uniform_color_buffer_data]);
-        base.device.unmap_memory(uniform_color_buffer_memory);
-        base.device
-            .bind_buffer_memory(uniform_color_buffer, uniform_color_buffer_memory, 0)
-            .expect("failed tp bind memory");
-    }
-    let image = image::load_from_memory(include_bytes!("../../assets/earthmap.jpg"))
-        .unwrap()
-        .to_rgba8();
-    let (width, height) = image.dimensions();
-    let image_extent = vk::Extent2D { width, height };
-    let image_data = image.into_raw();
-    let image_buffer_info = vk::BufferCreateInfo::builder()
-        .size(size_of::<u8>() as u64 * image_data.len() as u64)
-        .usage(vk::BufferUsageFlags::TRANSFER_SRC)
-        .sharing_mode(vk::SharingMode::EXCLUSIVE);
-    let image_buffer = unsafe {
-        base.device
-            .create_buffer(&image_buffer_info, None)
-            .expect("failed to get buffer")
-    };
-    let image_buffer_memory_req =
-        unsafe { base.device.get_buffer_memory_requirements(image_buffer) };
-    let image_buffer_memory_index = unsafe {
-        find_memory_type_index(
-            &image_buffer_memory_req,
-            &base.device_memory_properties,
-            vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-        )
-        .expect("failed to find memory index")
-    };
-    let image_buffer_allocation = allocator
-        .allocate(&AllocationCreateDesc {
-            name: "image buffer allocation",
-            requirements: image_buffer_memory_req,
-            location: MemoryLocation::CpuToGpu,
-            linear: true,
-        })
-        .expect("failed to make allocation");
-
-    let img_ptr = image_buffer_allocation
-        .mapped_ptr()
-        .expect("failed to map image pointer");
-    unsafe {
-        let mut image_slice = Align::new(
-            img_ptr.as_ptr(),
-            align_of::<u8>() as u64,
-            image_buffer_memory_req.size,
-        );
-        image_slice.copy_from_slice(&image_data);
-
-        base.device
-            .bind_buffer_memory(
-                image_buffer,
-                image_buffer_allocation.memory(),
-                image_buffer_allocation.offset(),
-            )
-            .expect("failed to bind texture memory");
-    }
-    let texture_create_info = vk::ImageCreateInfo::builder()
-        .image_type(vk::ImageType::TYPE_2D)
-        .format(vk::Format::R8G8B8A8_UNORM)
-        .extent(image_extent.into())
-        .mip_levels(1)
-        .array_layers(1)
-        .samples(vk::SampleCountFlags::TYPE_1)
-        .tiling(vk::ImageTiling::OPTIMAL)
-        .usage(vk::ImageUsageFlags::TRANSFER_DST | vk::ImageUsageFlags::SAMPLED)
-        .sharing_mode(vk::SharingMode::EXCLUSIVE);
-    let texture_image = unsafe {
-        base.device
-            .create_image(&texture_create_info, None)
-            .expect("failed to create image")
-    };
-    let texture_memory_req = unsafe { base.device.get_image_memory_requirements(texture_image) };
-    let texture_memory_index = unsafe {
-        find_memory_type_index(
-            &texture_memory_req,
-            &base.device_memory_properties,
-            vk::MemoryPropertyFlags::DEVICE_LOCAL,
-        )
-        .expect("failed to get memory index")
-    };
-    let texture_allocation = allocator
-        .allocate(&AllocationCreateDesc {
-            name: "image buffer allocation",
-            requirements: image_buffer_memory_req,
-            location: MemoryLocation::GpuOnly,
-            linear: true,
-        })
-        .expect("failed to free allocation");
-    let texture_alloc_info = vk::MemoryAllocateInfo::builder()
-        .allocation_size(texture_memory_req.size)
-        .memory_type_index(texture_memory_index);
-
-    unsafe {
-        base.device
-            .bind_image_memory(
-                texture_image,
-                texture_allocation.memory(),
-                texture_allocation.offset(),
-            )
-            .expect("failed to bind image memory");
-    }
-    unsafe {
-        record_submit_commandbuffer(
-            &base.device,
-            base.setup_command_buffer,
-            base.setup_commands_reuse_fence,
-            base.present_queue,
-            &[],
-            &[],
-            &[],
-            |device, texture_command_buffer| {
-                let texture_barrier = vk::ImageMemoryBarrier::builder()
-                    .dst_access_mask(vk::AccessFlags::TRANSFER_WRITE)
-                    .new_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
-                    .image(texture_image)
-                    .subresource_range(
-                        vk::ImageSubresourceRange::builder()
-                            .aspect_mask(vk::ImageAspectFlags::COLOR)
-                            .level_count(1)
-                            .layer_count(1)
-                            .build(),
-                    )
-                    .build();
-                device.cmd_pipeline_barrier(
-                    texture_command_buffer,
-                    vk::PipelineStageFlags::BOTTOM_OF_PIPE,
-                    vk::PipelineStageFlags::TRANSFER,
-                    vk::DependencyFlags::empty(),
-                    &[],
-                    &[],
-                    &[texture_barrier],
-                );
-                let buffer_copy_region = vk::BufferImageCopy::builder()
-                    .image_subresource(
-                        vk::ImageSubresourceLayers::builder()
-                            .aspect_mask(vk::ImageAspectFlags::COLOR)
-                            .layer_count(1)
-                            .build(),
-                    )
-                    .image_extent(image_extent.into())
-                    .build();
-                device.cmd_copy_buffer_to_image(
-                    texture_command_buffer,
-                    image_buffer,
-                    texture_image,
-                    vk::ImageLayout::TRANSFER_DST_OPTIMAL,
-                    &[buffer_copy_region],
-                );
-                let texture_barrier_end = vk::ImageMemoryBarrier::builder()
-                    .src_access_mask(vk::AccessFlags::TRANSFER_WRITE)
-                    .dst_access_mask(vk::AccessFlags::SHADER_READ)
-                    .old_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
-                    .new_layout(vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL)
-                    .image(texture_image)
-                    .subresource_range(
-                        vk::ImageSubresourceRange::builder()
-                            .aspect_mask(vk::ImageAspectFlags::COLOR)
-                            .level_count(1)
-                            .layer_count(1)
-                            .build(),
-                    )
-                    .build();
-                device.cmd_pipeline_barrier(
-                    texture_command_buffer,
-                    vk::PipelineStageFlags::TRANSFER,
-                    vk::PipelineStageFlags::FRAGMENT_SHADER,
-                    vk::DependencyFlags::empty(),
-                    &[],
-                    &[],
-                    &[texture_barrier_end],
-                );
-            },
-        )
-    }
-    allocator
-        .free(image_buffer_allocation)
-        .expect("failed to free");
-    let sampler_info = vk::SamplerCreateInfo::builder()
-        .mag_filter(vk::Filter::LINEAR)
-        .min_filter(vk::Filter::LINEAR)
-        .mipmap_mode(vk::SamplerMipmapMode::LINEAR)
-        .address_mode_u(vk::SamplerAddressMode::MIRRORED_REPEAT)
-        .address_mode_v(vk::SamplerAddressMode::MIRRORED_REPEAT)
-        .address_mode_w(vk::SamplerAddressMode::MIRRORED_REPEAT)
-        .max_anisotropy(1.0)
-        .border_color(vk::BorderColor::FLOAT_OPAQUE_WHITE)
-        .compare_op(vk::CompareOp::NEVER);
-    let sampler = unsafe {
-        base.device
-            .create_sampler(&sampler_info, None)
-            .expect("failed to get sampler")
-    };
-    let tex_image_view_info = vk::ImageViewCreateInfo::builder()
-        .view_type(vk::ImageViewType::TYPE_2D)
-        .format(texture_create_info.format)
-        .components(vk::ComponentMapping {
-            r: vk::ComponentSwizzle::R,
-            g: vk::ComponentSwizzle::G,
-            b: vk::ComponentSwizzle::B,
-            a: vk::ComponentSwizzle::A,
-        })
-        .subresource_range(
-            vk::ImageSubresourceRange::builder()
-                .aspect_mask(vk::ImageAspectFlags::COLOR)
-                .level_count(1)
-                .layer_count(1)
-                .build(),
-        )
-        .image(texture_image);
-    let tex_image_view = unsafe {
-        base.device
-            .create_image_view(&tex_image_view_info, None)
-            .expect("failed to get tex image view")
-    };
-
-    let desc_alloc_info = vk::DescriptorSetAllocateInfo::builder()
-        .descriptor_pool(descriptor_pool)
-        .set_layouts(&desc_set_layouts);
-    let descriptor_sets = unsafe {
-        base.device
-            .allocate_descriptor_sets(&desc_alloc_info)
-            .expect("failed to allocate desc layout")
-    };
-
-    let tex_descriptor = vk::DescriptorImageInfo {
-        image_layout: vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL,
-        image_view: tex_image_view,
-        sampler,
-    };
-    let write_desc_sets = [vk::WriteDescriptorSet {
-        dst_set: descriptor_sets[0],
-        dst_binding: 0,
-        descriptor_count: 1,
-        descriptor_type: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
-        p_image_info: &tex_descriptor,
-        ..Default::default()
-    }];
-
-    unsafe {
-        base.device.update_descriptor_sets(&write_desc_sets, &[]);
-    }
     let mut vertex_spv_file = Cursor::new(include_bytes!("../shaders/bin/push.vert.glsl"));
     let mut frag_spv_file = Cursor::new(include_bytes!("../shaders/bin/push.frag.glsl"));
     let vertex_code = read_spv(&mut vertex_spv_file).expect("failed tp read vertex shader code");
@@ -1062,7 +685,7 @@ pub fn run(base: &Base) {
                         vk::PipelineBindPoint::GRAPHICS,
                         pipeline_layout,
                         0,
-                        &descriptor_sets[..],
+                        &[t_mesh.descriptor_set],
                         &[],
                     );
                     device.cmd_bind_pipeline(
@@ -1075,7 +698,7 @@ pub fn run(base: &Base) {
                     device.cmd_bind_vertex_buffers(
                         draw_command_buffer,
                         0,
-                        &[vertex_input_buffer],
+                        &[t_mesh.vertex_buffer],
                         &[0],
                     );
                     device.cmd_push_constants(
@@ -1087,18 +710,11 @@ pub fn run(base: &Base) {
                     );
                     device.cmd_bind_index_buffer(
                         draw_command_buffer,
-                        index_buffer,
+                        t_mesh.index_buffer,
                         0,
                         vk::IndexType::UINT32,
                     );
-                    device.cmd_draw_indexed(
-                        draw_command_buffer,
-                        mesh.indices.len() as u32,
-                        1,
-                        0,
-                        0,
-                        1,
-                    );
+                    device.cmd_draw_indexed(draw_command_buffer, t_mesh.num_indices, 1, 0, 0, 1);
                     device.cmd_end_render_pass(draw_command_buffer);
                 },
             );
@@ -1122,28 +738,13 @@ pub fn run(base: &Base) {
             .destroy_shader_module(vertex_shader_module, None);
         base.device.destroy_shader_module(frag_shader_module, None);
         t_mesh.free_resources(base, &mut allocator);
-        base.device.destroy_buffer(image_buffer, None);
-        allocator
-            .free(texture_allocation)
-            .expect("failed to free texture allocation");
 
-        base.device.destroy_image_view(tex_image_view, None);
-        base.device.destroy_image(texture_image, None);
-
-        allocator.free(index_buffer_memory).expect("failed to free");
-
-        base.device.destroy_buffer(index_buffer, None);
-        base.device.free_memory(uniform_color_buffer_memory, None);
-        base.device.destroy_buffer(uniform_color_buffer, None);
-        allocator.free(vertex_buffer_allocation);
-
-        base.device.destroy_buffer(vertex_input_buffer, None);
         for &descriptor_set_layout in desc_set_layouts.iter() {
             base.device
                 .destroy_descriptor_set_layout(descriptor_set_layout, None);
         }
         base.device.destroy_descriptor_pool(descriptor_pool, None);
-        base.device.destroy_sampler(sampler, None);
+
         for framebuffer in framebuffers {
             base.device.destroy_framebuffer(framebuffer, None);
         }
