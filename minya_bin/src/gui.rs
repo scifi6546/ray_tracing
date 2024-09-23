179a511950717154ae91aa602c84b91ef1bf4d6d
Nicholas Alexeev <nickalexeev@protonmail.com>
2024-09-22 19:20:48 -0800

    updated gui layout

--- minya_bin/src/gui.rs
+++ minya_bin/src/gui.rs
@@ -25,52 +25,53 @@ impl GuiCtx {
     }
     pub fn update(&mut self, ctx: &mut miniquad::Context) {
         self.egui_mq.run(ctx, |_mq_ctx, egui_ctx| {
-            egui::Window::new("Scenarios").show(egui_ctx, |ui| {
-                ui.heading("Choose Scenario: ");
-                for scenario in self.scenarios.iter() {
-                    if ui.button(scenario).clicked() {
-                        if let Some(err) = self
-                            .message_chanel
-                            .send(Message::LoadScenario(scenario.clone()))
-                            .err()
-                        {
-                            error!("failed to load scenario: {:?}", err);
-                        }
-                        println!("clicked: {}", scenario);
-                    }
-                }
-            });
-            egui::Window::new("Save File").show(egui_ctx, |ui| {
-                ui.separator();
-                ui.label(egui::RichText::new("Save").heading());
-                if ui.button("Save Render").clicked() {
-                    info!("saving file");
-                    if let Some(save_path) = rfd::FileDialog::new().save_file() {
-                        if let Some(err) = self
-                            .message_chanel
-                            .send(Message::SaveFile(save_path.clone()))
-                            .err()
-                        {
-                            error!(
+            egui::TopBottomPanel::top("top_menu").show(egui_ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Save Render").clicked() {
+                            info!("saving file");
+                            if let Some(save_path) = rfd::FileDialog::new().save_file() {
+                                if let Some(err) = self
+                                    .message_chanel
+                                    .send(Message::SaveFile(save_path.clone()))
+                                    .err()
+                                {
+                                    error!(
                                 "failed to save file because rendering thread crashed, error: {:?}",
                                 err
                             )
+                                }
+                                info!("saving file to path: {:?}", save_path);
+                            } else {
+                                info!("file save canceled");
+                            }
                         }
-                        info!("saving file to path: {:?}", save_path);
-                    } else {
-                        info!("file save canceled");
-                    }
-                }
-                ui.separator();
-                ui.label(egui::RichText::new("Set Shader").heading());
-                let shaders = CurrentShader::names();
-                for s in shaders {
-                    if ui.button(&s).clicked() {
-                        self.message_chanel
-                            .send(Message::SetShader(CurrentShader::from_str(&s)))
-                            .expect("failed to send");
-                    }
-                }
+                    });
+                    ui.menu_button("Scenarios", |ui| {
+                        for scenario in self.scenarios.iter() {
+                            if ui.button(scenario).clicked() {
+                                if let Some(err) = self
+                                    .message_chanel
+                                    .send(Message::LoadScenario(scenario.clone()))
+                                    .err()
+                                {
+                                    error!("failed to load scenario: {:?}", err);
+                                }
+                                println!("clicked: {}", scenario);
+                            }
+                        }
+                    });
+                    ui.menu_button("Shader", |ui| {
+                        let shaders = CurrentShader::names();
+                        for s in shaders {
+                            if ui.button(&s).clicked() {
+                                self.message_chanel
+                                    .send(Message::SetShader(CurrentShader::from_str(&s)))
+                                    .expect("failed to send");
+                            }
+                        }
+                    });
+                });
             });
 
             egui::Window::new("Log")
